e14944782af6462c769b00382d7bd462864df4e9
krb3p <ryankatherineburke@gmail.com>
2017-02-18 19:29:32 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+
+
+class Student < User
+
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
+
+  def knowledge
+    @knowledge 
+  end
 
 end
